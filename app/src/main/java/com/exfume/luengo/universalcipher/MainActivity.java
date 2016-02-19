313b92f4c9b3be8466a55720f2eadb3670304ffb
Benjides <benjergara@gmail.com>
2016-02-19 23:48:36 +0100

    Created Navigation Drawer

--- app/src/main/java/com/exfume/luengo/universalcipher/MainActivity.java
+++ app/src/main/java/com/exfume/luengo/universalcipher/MainActivity.java
@@ -1,60 +1,92 @@
 package com.exfume.luengo.universalcipher;
 
-import android.provider.Telephony;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.exfume.luengo.universalcipher.fragments.fragment_vigenere;
+
+/**
+ * Created by Benjamin on 19/02/2016.
+ */
 public class MainActivity extends AppCompatActivity {
-    private Button Submit;
-    private EditText Message;
-    private EditText Key;
-    private Cipher Cipher;
 
-
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Submit = (Button) findViewById(R.id.submit);
-        Message = (EditText) findViewById(R.id.message);
-        Key = (EditText) findViewById(R.id.key);
-        Cipher = new Cipher();
-        Submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        setToolbar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setDrawer(navigationView);
+            selectItem(navigationView.getMenu().getItem(0));
+        }
+    }
 
-                Vigenere();
-
-            }
-        });
-
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
     }
 
-    public void Vigenere(){
-
-        TextView result = (TextView) findViewById(R.id.result);
-        try {
-            Cipher.setKey(Key.getText().toString());
-            Log.i("VIGENERE", Message.getText().toString());
-            Cipher.setText(Message.getText().toString());
-            Log.i("VIGKEY", Key.getText().toString());
-            Log.i("VIGRESULT", Cipher.Vigenere());
-            result.setText(Cipher.Vigenere());
-
-
-        }
-        catch (Exception e){
-            result.setText("Fill everthing, biatch");
-            Message.setHighlightColor(0xFFFF33);
-        }
-
+    private void setDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        selectItem(menuItem);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
 
     }
+
+    private void selectItem(MenuItem itemDrawer) {
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.Vigenere:
+                fragment = new fragment_vigenere();
+                break;
+        }
+        if (fragment != null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_frame , fragment)
+                    .commit();
+        }
+
+        // Setear título actual
+        setTitle(itemDrawer.getTitle());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
---
+++ app/src/main/java/com/exfume/luengo/universalcipher/fragments/fragment_vigenere.java
@@ -0,0 +1,61 @@
+package com.exfume.luengo.universalcipher.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.exfume.luengo.universalcipher.Cipher;
+import com.exfume.luengo.universalcipher.R;
+
+/**
+ * Created by Benjamin on 19/02/2016.
+ */
+public class fragment_vigenere extends Fragment {
+
+    private Button mButton;
+    private TextInputLayout mMessage;
+    private TextInputLayout mKey;
+    private TextView result;
+    private Cipher cipher;
+
+    public fragment_vigenere(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.vigenere_cipher, container, false);
+        cipher = new Cipher();
+        mButton = (Button) view.findViewById(R.id.submit);
+        mMessage = (TextInputLayout) view.findViewById(R.id.mMessage);
+        mKey = (TextInputLayout) view.findViewById(R.id.mKey);
+        result = (TextView) view.findViewById(R.id.mResult);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vigenere();
+            }
+        });
+        return view;
+    }
+
+    private void Vigenere(){
+        try {
+            cipher.setKey(mKey.getEditText().getText().toString());
+            cipher.setText(mMessage.getEditText().getText().toString());
+            result.setText(cipher.Vigenere());
+        }
+        catch (Exception e){
+            result.setText(e.getMessage());
+        }
+    }
+
+}
